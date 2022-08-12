d2e740922a3f2cd7f2950c12fea52ffd54d0ed44
2manycooks <73556731+2manycooks@users.noreply.github.com>
2022-08-12 10:19:49 -0700

    radio refactor

--- js/script.js
+++ js/script.js
@@ -5,36 +5,68 @@ let fToC = document.querySelector('#fToC')
 let cToF = document.querySelector('#cToF')
 let clear = document.querySelector('#clear')
 let result = document.querySelector('#result')
+let fToCRadio = document.querySelector('#radio1')
+let cToFRadio = document.querySelector('#radio2')
+let submit = document.querySelector('#submit')
+
 
 // adding event listeners
 
-fToC.addEventListener("click", function(e) {
-    let preConversion = input.value
-    let postConversion = (preConversion - 32) * (5/9)
-    result.innerText = postConversion
+// fToC.addEventListener("click", function(e) {
+//     let preConversion = input.value
+//     let postConversion = (preConversion - 32) * (5/9)
+//     result.innerText = postConversion
 
-    if (postConversion <= 0) {
-        document.body.style.backgroundImage = "url('https://media.giphy.com/media/xTeV7qibUsOYA3mmYw/giphy.gif')";
-    } else if (postConversion > 38) {
-        document.body.style.backgroundImage = "url('https://media.giphy.com/media/P7JmDW7IkB7TW/giphy.gif')";
+//     if (postConversion <= 0) {
+//         document.body.style.backgroundImage = "url('https://media.giphy.com/media/xTeV7qibUsOYA3mmYw/giphy.gif')";
+//     } else if (postConversion > 38) {
+//         document.body.style.backgroundImage = "url('https://media.giphy.com/media/P7JmDW7IkB7TW/giphy.gif')";
+//     }
+// })
+
+// cToF.addEventListener("click", function(e) {
+//     let preConversion = input.value
+//     let postConversion = (preConversion * 1.8) + 32
+//     result.innerText = postConversion
+
+//     if (postConversion <= 32) {
+//         document.body.style.backgroundImage = "url('https://media.giphy.com/media/xTeV7qibUsOYA3mmYw/giphy.gif')";
+//     } else if (postConversion > 100) {
+//         document.body.style.backgroundImage = "url('https://media.giphy.com/media/P7JmDW7IkB7TW/giphy.gif')";
+//     }
+// })
+
+
+submit.addEventListener("click", function(e) {
+    if(fToCRadio.checked == true) {
+        let preConversion = input.value
+        let postConversion = (preConversion - 32) * (5/9)
+        result.innerText = postConversion
+
+        if (postConversion <= 0) {
+                    document.body.style.backgroundImage = "url('https://media.giphy.com/media/xTeV7qibUsOYA3mmYw/giphy.gif')";
+                } else if (postConversion > 38) {
+                    document.body.style.backgroundImage = "url('https://media.giphy.com/media/P7JmDW7IkB7TW/giphy.gif')";
+                }
+    } else if (cToFRadio.checked == true) {
+        let preConversion = input.value
+        let postConversion = (preConversion * 1.8) + 32
+        result.innerText = postConversion
+
+        if (postConversion <= 32) {
+                    document.body.style.backgroundImage = "url('https://media.giphy.com/media/xTeV7qibUsOYA3mmYw/giphy.gif')";
+                } else if (postConversion > 100) {
+                    document.body.style.backgroundImage = "url('https://media.giphy.com/media/P7JmDW7IkB7TW/giphy.gif')";
+                }
     }
 })
 
-cToF.addEventListener("click", function(e) {
-    let preConversion = input.value
-    let postConversion = (preConversion * 1.8) + 32
-    result.innerText = postConversion
+// clear.addEventListener("click", function(e) {
+//     result.innerText = ''
+//     input.value = ''
+//     document.body.style.backgroundImage = ''
+// })
 
-    if (postConversion <= 32) {
-        document.body.style.backgroundImage = "url('https://media.giphy.com/media/xTeV7qibUsOYA3mmYw/giphy.gif')";
-    } else if (postConversion > 100) {
-        document.body.style.backgroundImage = "url('https://media.giphy.com/media/P7JmDW7IkB7TW/giphy.gif')";
-    }
+fToCRadio.addEventListener("click", function(e) {
+    console.log(e)
 })
-
-clear.addEventListener("click", function(e) {
-    result.innerText = ''
-    input.value = ''
-    document.body.style.backgroundImage = ''
-})
-
